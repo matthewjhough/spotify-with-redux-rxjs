44624ceef8e0a2e425c1c2fb0c1101150d5385d6
Matthew Hough <matthough1@icloud.com>
2019-01-27 19:29:32 -0500

    store types as variable

--- src/ducks/search.js
+++ src/ducks/search.js
@@ -2,11 +2,12 @@ import { createRequest } from '../utils';
 
 export const SEARCH_ACTION = 'SEARCH_ACTION';
 export const SEARCH_URL = 'https://api.spotify.com/v1/search';
+export const RESULT_TYPES = 'type=artist,track,album';
 
 const initialState = {};
 
 export function searchAction(query) {
-  const url = `${SEARCH_URL}?q=${query}&type=artist,track,album`;
+  const url = `${SEARCH_URL}?q=${query}&${RESULT_TYPES}`;
   const method = 'GET';
   return function(dispatch) {
     createRequest(url, method).then(results => {
